40acf9f8f971a3bcb5898cd81cf70f7f3421fe13
Garett Roberts <mygarett@gmail.com>
2017-03-28 22:48:52 -0700

    moved functions to .h

--- crackingthecodeinterview/stacks_queues/stack.cxx
+++ crackingthecodeinterview/stacks_queues/stack.cxx
@@ -1,98 +1,16 @@
 #include "stack.h"
 
 //Pops the head from the stack, prints the popped value/desiredostream if desired
-template<typename T>
-Node<T>*
-Stack<T>::popHead(bool out)
-{
-    Node<T> *popped = head;
-            head    = popped->next;
-    
-    if(out)
-        cout << "Popped value is: " << head->value << endl; 
-    
-    size--;
 
-    return popped;
-}
-
-//Prints the head of the stack into the given ostream
-template<typename T>
-void 
-Stack<T>::readHead(ostream &os)
-{
-    os << "Head is: " << head->value << endl;
-}
-
-//Pushes a node onto the stack Returns 0 if failure; 1 on success
-template<typename T>
 int
-Stack<T>::pushNode(Node<T> &in)
+main()
 {
-    in.next = head;
-    head    = &in; 
+    Stacki ourstack;
 
-    if(in == NULL)
-        return 0; 
+    ourstack.pushType(5);
+    ourstack.pushType(10);
 
-    if(*head != in)
-        return 0;
+    cout << ourstack << endl;
 
-    size++;
-
-    return 1;
-}
-
-template<typename T>
-void
-Stack<T>::pushType(T in)
-{
-    Node<T> *newnode = (Node<T>*) new Node<T>;
-
-    newnode->value = in;
-    newnode->next  = head;
-    head           = newnode;
-    
-    size++;
-
-}
-
-template<typename T>
-inline bool Stack<T>::operator==(const Stack<T>& in) const
-{  
-    Node<T> *firstnode = in.popHead(); 
-    Node<T> *currentno = head;
-
-    in.pushNode(firstnode);
-
-    if(currentno == NULL && firstnode == NULL)
-        return true;
-
-    while(firstnode != NULL && currentno != NULL){
-        if(currentno != firstnode)
-            return false;
-
-        currentno  = currentno->next;
-        firstnode = firstnode->next;
-    }         
-
-    if(!(currentno != NULL) != !(firstnode != NULL))
-            return false;
-
-    return true;
-} 
-
-template <typename T>
-inline void
-Stack::print(ostream &os)
-{
-    Node<T> *node = in;
-    in.pushNode(in);
-    while(node != NULL){
-        os << node << " ";
-        node = node->next;
-    }
-
-    return os;
-
+    return 0;
 }
--- crackingthecodeinterview/stacks_queues/stack.h
+++ crackingthecodeinterview/stacks_queues/stack.h
@@ -60,4 +60,101 @@ class Stack{
 typedef Stack<int>  Stacki;
 typedef Stack<char> Stackc;
 
+template<typename T>
+Node<T>*
+Stack<T>::popHead(bool out)
+{
+    Node<T> *popped = head;
+            head    = popped->next;
+    
+    if(out)
+        cout << "Popped value is: " << head->value << endl; 
+    
+    size--;
+
+    return popped;
+}
+
+//Prints the head of the stack into the given ostream
+template<typename T>
+void 
+Stack<T>::readHead(ostream &os)
+{
+    os << "Head is: " << head->value << endl;
+}
+
+//Pushes a node onto the stack Returns 0 if failure; 1 on success
+template<typename T>
+int
+Stack<T>::pushNode(Node<T> &in)
+{
+    in.next = head;
+    head    = &in; 
+
+    if(in == NULL)
+        return 0; 
+
+    if(*head != in)
+        return 0;
+
+    size++;
+
+    return 1;
+}
+
+template<typename T>
+void
+Stack<T>::pushType(T in)
+{
+    Node<T> *newnode = (Node<T>*) new Node<T>;
+
+    newnode->value = in;
+    newnode->next  = head;
+    head           = newnode;
+    
+    size++;
+
+}
+
+template<typename T>
+inline bool Stack<T>::operator==(const Stack<T>& in) const
+{  
+    Node<T> *firstnode = in.popHead(); 
+    Node<T> *currentno = head;
+
+    in.pushNode(firstnode);
+
+    if(currentno == NULL && firstnode == NULL)
+        return true;
+
+    while(firstnode != NULL && currentno != NULL){
+        if(currentno != firstnode)
+            return false;
+
+        currentno  = currentno->next;
+        firstnode = firstnode->next;
+    }         
+
+    if(!(currentno != NULL) != !(firstnode != NULL))
+            return false;
+
+    return true;
+} 
+
+template <typename T>
+ostream 
+&operator<<(ostream &os, const Stack<T>& in)
+
+{
+    Node<T> *node = in.popHead();
+    in.pushNode(in);
+    while(node != NULL){
+        os << node << " ";
+        node = node->next;
+    }
+
+    return os;
+
+}
+
 #endif
